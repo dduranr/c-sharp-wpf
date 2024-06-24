aba67aa32cc2fd5fd7ac372471082a5b43f53f27
David <official.dduran@gmail.com>
2024-06-24 08:47:32 -0600

    Agrego redimensión a dashboard window

--- App.xaml.cs
+++ App.xaml.cs
@@ -10,21 +10,17 @@ namespace WPF_LoginForm
     {
         protected void ApplicationStart(object sender, StartupEventArgs e)
         {
-            //var loginView = new LoginView();
-            //loginView.Show();
-            //loginView.IsVisibleChanged += (s, ev) =>
-            //{
-            //    if (loginView.IsVisible == false && loginView.IsLoaded)
-            //    {
-            //        var mainView = new MainView();
-            //        mainView.Show();
-            //        loginView.Close();
-            //    }
-            //};
             var loginView = new LoginView();
-            var mainView = new MainView();
-            mainView.Show();
-            loginView.Close();
+            loginView.Show();
+            loginView.IsVisibleChanged += (s, ev) =>
+            {
+                if (loginView.IsVisible == false && loginView.IsLoaded)
+                {
+                    var mainView = new MainView();
+                    mainView.Show();
+                    loginView.Close();
+                }
+            };
         }
     }
 }
--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -33,5 +33,21 @@ namespace WPF_LoginForm.Views
         {
             MessageBox.Show("Iniciando sesión...");
         }
+
+        /// <summary>
+        /// Este método gestiona la siguiente funcionalidad: Si el foco está sobre el campo USUARIO del formulario y se da enter, entonces se ejecuta la función asignada al submit.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TxtUser_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (btnLogin.Command != null && btnLogin.Command.CanExecute(null))
+                {
+                    btnLogin.Command.Execute(null);
+                }
+            }
+        }
     }
 }
